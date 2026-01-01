1590a4305862175ec3d5733f0b339a904cd2f46c
Diego Lopes Sakata <dihsakata@gmail.com>
2026-01-01 09:32:27 -0300

    feat: adiciona o product controller

---
+++ product/product/src/main/java/com/farmtech/product/controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.farmtech.product.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.farmtech.product.entity.Product;
+import com.farmtech.product.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product create(@RequestBody Product product) {
+        return service.create(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product product) {
+        return service.update(id, product);
+    }
+
+    @GetMapping
+    public List<Product> list() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+}
